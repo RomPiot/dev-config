b558e15254ce9e98b9577c516f0126fbff309ede
Romain PIOT <romainpiot.pro@gmail.com>
2023-06-18 22:55:13 +0200

    chore: update aliases

--- scripts/git_config.sh
+++ scripts/git_config.sh
@@ -1,11 +1,12 @@
 git config --global init.defaultBranch master
 git config --global core.autocrlf false
 git config --global core.eol lf
-git config --global alias.co checkout
+git config --global safe.directory '*'
+git config --global alias.co "checkout"
 git config --global alias.br "co -b"
-git config --global alias.com commit
+git config --global alias.com "commit -m"
 git config --global alias.amend "commit --amend --no-edit"
-git config --global alias.st status
+git config --global alias.st "status"
 git config --global alias.unstage "reset HEAD --"
 git config --global alias.last "log -1 HEAD"
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
